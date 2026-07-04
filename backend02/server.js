3a960f49634d13a5e33d9c15924907932cdff8a1
Aayush Kumar <ayushankitaara@gmail.com>
2026-07-04 15:00:32 +0530

    understood REST API and its methods

---
+++ backend02/server.js
@@ -0,0 +1,6 @@
+// server ko start karna
+const app = require("./src/app")
+
+app.listen(3000, ()=>{
+    console.log("server is runnimg on port 3000")
+})
---
+++ backend02/src/app.js
@@ -0,0 +1,47 @@
+// server ko create karna 
+const express = require("express");
+
+const app = express();
+app.use(express.json()) 
+
+const notes = []
+
+app.post("/notes",(req,res)=>{
+   
+    notes.push(req.body)
+
+    res.status(201).json({
+        message: "note added successfully",
+    })
+
+})
+
+
+app.get("/notes",(req,res)=>{
+
+     res.status(200).json({
+        message: "notes fetched successfully",
+         notes: notes
+    })
+})
+
+            // /notes/2 (:ke baad wala dynamic hoga)
+app.delete("/notes/:index", (req,res)=>{
+                const index = req.params.index
+                delete notes[index]
+                res.status(200).json({
+                    message: "notes deleted successfully"
+                })
+})
+
+
+app.patch("/notes/:index",(req,res)=>{
+    const index = req.params.index
+    const description = req.body.description
+    notes[index].description = description
+    res.status(200).json({
+        message:"note updated successfully"
+    })
+})
+
+module.exports = app
