83e7e588ea8f026619c9bcbe642a77c8ae9da7fc
Aayush Kumar <ayushankitaara@gmail.com>
2026-07-07 21:06:32 +0530

    learned authentication system and understood JWT toekns and about cookie storage

---
+++ backend05/src/controller/auth.controller.js
@@ -0,0 +1,28 @@
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const cookie = require('cookie-parser')
+
+
+async function registerUser(req, res){
+    const {username , email, password } = req.body
+
+    const user = await userModel.create({
+        username, email, password
+    })
+
+    const token = jwt.sign({
+        id: user._id,}, process.env.JWT_SECRET
+)
+
+res.cookie("token",token)
+
+res.status(201).json({
+    message:"User registered successfully",
+    user
+     
+
+
+})
+
+}
+module.exports = {registerUser}
